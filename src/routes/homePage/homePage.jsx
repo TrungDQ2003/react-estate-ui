9e354af6ffe3428eb6ac9b7610bcf0d8c371d2e5
TrungDQ2003 <doqt2003@gmail.com>
2025-03-03 11:49:30 +0700

    Finished Home Page

---
+++ src/routes/homePage/homePage.jsx
@@ -0,0 +1,36 @@
+import SearchBar from '../../components/searchBar/SearchBar'
+import './homePage.scss'
+
+function HomePage(){
+  return (
+    <div className='homePage'>
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur est vel cum. Dicta nemo magnam nesciunt perferendis dolorem facilis sequi.
+          </p>
+          <SearchBar/>
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>1200+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
